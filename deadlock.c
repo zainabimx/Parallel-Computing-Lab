1af27c48e0e85f9577f75d426c4f6c262cd9dd29
zainabimx <153424664+zainabimx@users.noreply.github.com>
2025-09-08 10:11:15 +0530

    Add files via upload

---
+++ deadlock.c
@@ -0,0 +1,19 @@
+#include <stdio.h> 
+#include <mpi.h>
+int main(int argc, char *argv[]) {
+    int rank, data_send, data_recv;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    data_send = rank;
+    if (rank == 0) {
+        MPI_Send(&data_send, 1, MPI_INT, 1, 0, MPI_COMM_WORLD);
+        MPI_Recv(&data_recv, 1, MPI_INT, 1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Process %d received %d\n", rank, data_recv);
+    } else if (rank == 1) {
+        MPI_Recv(&data_recv, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&data_send, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+        printf("Process %d received %d\n", rank, data_recv);
+    }
+    MPI_Finalize();
+    return 0;
+}
